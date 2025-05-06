e6a00f6b888f7e93025acf27bc74beda40a0588c
Dawit Fikru Amanu <58340792+dawitfamanu@users.noreply.github.com>
2025-05-06 15:25:42 +0900

    Import global styles in the main application component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { SolanaWalletProvider } from '../contexts/SolanaWalletProvider';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
