5c603eb3294ff1f2693e2351a61f59f426b7f336
devJZen <quasar.joo@google.com>
2025-05-06 14:36:38 +0900

    eslint config에 prettier config 추가가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,13 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import prettier from "eslint-config-prettier"
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"],
-    plugins: { js, prettier: "eslint-plugin-prettier" }, 
-    extends: [
-      "js/recommended", 
-      "plugin:prettier/recommended",  // Prettier를 ESLint와 통합하는 추천 설정
-      "prettier"  // Prettier와 충돌하는 ESLint 규칙을 비활성화
-    ] 
-  },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts, tsx}"],
+    plugins : { js },
+    languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
+  prettier,
 ]);
